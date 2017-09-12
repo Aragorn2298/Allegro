4715d15e83b0ef7819548d13ae524e1a2a52d37e
Aragorn2209 <ramonbust2209@gmail.com>
2017-09-12 10:25:10 -0600

    Menu
    Menu

--- main.cpp
+++ main.cpp
@@ -12,41 +12,42 @@ int main ()
         BITMAP *fondo2 = load_bitmap("Fondo2.bmp",NULL);
         BITMAP *fondo3 = load_bitmap("Fondo3.bmp",NULL);
         BITMAP *fondo4 = load_bitmap("Fondo4.bmp",NULL);
-        BITMAP *cursor = load_bitmap("cursor.png",NULL);
+        BITMAP *cursor = load_bitmap("cursor.bmp",NULL);
 
         bool salida = false;
         while(!salida)
         {
-            if(mouse_x > 225 && mouse_x < 106 && mouse_y > 1360 && mouse_y < 799)
+            if(mouse_x > 170 && mouse_x < 395 &&
+                mouse_y > 386 && mouse_y < 488)
             {
                 blit(fondo2,buffer,0,0,0,0,1360,799);
                 if(mouse_b & 1)
                 {
                     salida=true;
-                }else if(mouse_x > 508 && mouse_x <110 && mouse_y >1360 && mouse_y < 799)
+                }
+            }else if(mouse_x > 176 && mouse_x <681 &&
+                         mouse_y >503 && mouse_y < 603)
                 {
                     blit(fondo3,buffer,0,0,0,0,1360,799);
                     if(mouse_b & 1)
                     {
                     salida=true;
                     }
-                }else if(mouse_x>191 && mouse_x < 95 && mouse_y >1360 && mouse_y < 799)
+                }else if(mouse_x>177 && mouse_x < 367 &&
+                         mouse_y >643 && mouse_y < 735)
                 {
                     blit(fondo4,buffer,0,0,0,0,1360,799);
                     if(mouse_b & 1)
                     {
                     salida=true;
                     }
-                }else {
-                    blit(fondo1,buffer,0,0,0,0,1360,799);
-                }
+                }else blit(fondo1,buffer,0,0,0,0,1360,799);
+
                 masked_blit(cursor,buffer,0,0,mouse_x,mouse_y,330,418);
                 blit(buffer,screen,0,0,0,0,1360,799);
-        }
-        destroy_bitmap(buffer);
+
     }
+    destroy_bitmap(buffer);
     return 0;
 }
 END_OF_MAIN ();
-
-
